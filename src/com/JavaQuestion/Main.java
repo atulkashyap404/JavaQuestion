79a94c56f4b24dd97287cde9173af2521be045b8
atulkumarmanjhi <atulkashyapvlog@gmail.com>
2021-10-21 20:26:29 +0530

    First Question find the number greater one between two number

---
+++ src/com/JavaQuestion/Main.java
@@ -0,0 +1,17 @@
+package com.JavaQuestion;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //wap program to find the large of two number.
+        int a = 10, b = 20;
+        if(a>b)
+        {
+            System.out.println("A is greater");
+        }
+        else
+        {
+            System.out.println("B is greater");
+        }
+    }
+}
