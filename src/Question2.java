d9b2d10f7f1890a47aa2145f4aa46219866e5c47
atulkumarmanjhi <atulkashyapvlog@gmail.com>
2021-10-21 20:34:14 +0530

    checking greater in three number

---
+++ src/Question2.java
@@ -0,0 +1,16 @@
+
+
+public class Question2 {
+    public static void main(String[] args) {
+        int a = 133, b = 233, c = 44;
+        if(a>b && a>c){
+            System.out.println("a is greater");
+        }
+        else if(b>a && b>c){
+            System.out.println("b is greater");
+        }
+        else {
+            System.out.println("c is greater");
+        }
+    }
+}
